12a3b7478c1b459170c2c923131d128cf0da788f
Ash Kestrel <tetrapteryxgames@gmail.com>
2016-03-15 14:29:48 -0600

    :star2: Select star and return to galaxy view

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,16 @@
 var currentMap;
+var saveMainMap;
 var zooming;
 var shift = false;
+var raycaster = new THREE.Raycaster();
+var cursor = new THREE.Vector2();
 
 renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 $(document).ready(function() {
-	var galaxyMap = new Map();
+	var galaxyMap = new Map('galaxy');
 	$.get('http://localhost:3000/stars')
 	.done(function(stars) {
 		var sprite = new THREE.CanvasTexture(starSprite());
@@ -18,6 +21,12 @@ $(document).ready(function() {
 		starGeometry.colors = stars.map(function(star) {
 			var hsl = 'hsl(' + star.h + ', ' + Math.abs(star.s) + '%, ' + star.l + '%)';
 			return new THREE.Color(hsl);
+		});
+		starGeometry.label = stars.map(function(star) {
+			return star.name;
+		});
+		starGeometry.starId = stars.map(function(star) {
+			return star.id;
 		});
 
 		var starMaterial = new THREE.PointsMaterial({
@@ -37,21 +46,69 @@ $(document).ready(function() {
 		galaxyMap.camera.position.z = 200;
 		currentMap = galaxyMap;
 
+		currentMap.lastSelected = false;
+
 		createEventListeners();
 
 		render();
 	});
 });
 
-function Map() {
+function Map(type) {
+	this.type = type;
 	this.scene = new THREE.Scene();
 	this.scene.fog = new THREE.FogExp2(0x5500dd, 0.01);
 	this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 }
 
+function setUpStarMap (star_id) {
+	var starMap = new Map('star');
+	switchMap(starMap);
+	$('.label').text(star_id);
+	$('.back').css('display', 'block');
+	$('.zoom-controls').css('display', 'none');
+}
+
+function switchMap (map) {
+	if (map) {
+		saveMainMap = currentMap;
+		currentMap = map;
+	} else {
+		currentMap = saveMainMap;
+		$('.zoom-controls').css('display', 'block');
+		$('.back').css('display', 'none');
+	}
+}
 
 function render () {
 	requestAnimationFrame( render );
+
+	if (currentMap.type == 'galaxy') {
+		raycaster.setFromCamera( cursor, currentMap.camera, 0, 1);	
+
+		var selected = raycaster.intersectObjects(currentMap.scene.children, true);
+
+		if (selected.length) {
+			currentMap.selected = selected[0];
+
+			if (currentMap.lastSelected !== currentMap.selected.index) {
+				$('.label').text(currentMap.stars.geometry.label[currentMap.selected.index]);
+				if (currentMap.lastSelectedColor) {
+					currentMap.stars.geometry.colors[currentMap.lastSelected].set(currentMap.lastSelectedColor);
+				}
+				currentMap.lastSelectedColor = new THREE.Color().copy(currentMap.selected.object.geometry.colors[currentMap.selected.index]);
+			}
+
+			currentMap.selected.object.geometry.colors[currentMap.selected.index].set( 0x55eeff );
+			currentMap.lastSelected = currentMap.selected.index;
+
+			currentMap.stars.geometry.colorsNeedUpdate = true;
+
+		} else {
+			currentMap.selected = false;
+		}
+
+	}
 
 	renderer.render(currentMap.scene, currentMap.camera);
 }
@@ -86,10 +143,18 @@ function createEventListeners() {
 	});
 	$('canvas').on('mousedown', function(event) {
 		if (shift) {
-			getMouseMovement({x: event.clientX, y: event.clientY});
-		} else {
-			console.log('nope');
+			trackDragging({x: event.clientX, y: event.clientY});
+		} else if (currentMap.selected) {
+			setUpStarMap(currentMap.stars.geometry.starId[currentMap.selected.index]);
 		}
+	});
+	$(window).on('mousemove', function(event) {
+		cursor.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+		cursor.y = - ( event.clientY / window.innerHeight ) * 2 + 1;	
+	});
+
+	$('.back').on('click', function(event) {
+		switchMap();
 	});
 
 	$(window).on('mouseup', function(event) {
@@ -102,7 +167,7 @@ function createEventListeners() {
 	});
 }
 
-function getMouseMovement(oldCoords) {
+function trackDragging(oldCoords) {
 	$('canvas').on('mousemove', function(event){
 		var deltaX = oldCoords.x - event.clientX;
 		var deltaY = oldCoords.y - event.clientY;
@@ -110,7 +175,7 @@ function getMouseMovement(oldCoords) {
 		currentMap.camera.position.y -= deltaY/10;
 		$('canvas').off('mousemove');
 		if(shift) {
-			getMouseMovement({x: event.clientX, y: event.clientY});
+			trackDragging({x: event.clientX, y: event.clientY});
 		}
 	});
 }
@@ -140,9 +205,9 @@ function zoom (direction, pan) {
 		zooming = setInterval(function(){
 			if(pan) {
 				if (pan > 0) {
-				currentMap.camera.position.x -= direction;
+					currentMap.camera.position.x -= direction;
 				} else {
-				currentMap.camera.position.y += direction;
+					currentMap.camera.position.y += direction;
 				}
 			} else if(direction > 0 && currentMap.camera.position.z >= 100 || direction < 0 && currentMap.camera.position.z <= 300) {
 				currentMap.camera.position.z -= direction;
